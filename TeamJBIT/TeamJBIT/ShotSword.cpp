6a18359ec8b1d740b7541f75c44f4e782984b951
wendorfj <wendorfj@7c7f1771-d677-4e88-9638-a76dd5717754>
2013-06-04 04:04:20 +0000

    Fixed shotSword.

--- TeamJBIT/TeamJBIT/ShotSword.cpp
+++ TeamJBIT/TeamJBIT/ShotSword.cpp
@@ -25,7 +25,16 @@ void ShotSword::update(float deltaTime, sf::Vector2f pos, std::list<Bullet*>& pl
 	if(currentAngle >= 0 && shotDone == false)
 	{
 		sf::Vector2f vel(dir.x * 400, dir.y*400);
+
+		float angle = -atan2f(dir.x, dir.y);
+		float yOffset = sin(angle);
+		float xOffset = cos(angle);
+
 		playerBullets.push_back(new Bullet("media/bullet.png", pos, vel));
+		playerBullets.push_back(new Bullet("media/bullet.png", sf::Vector2f(pos.x + (15.f*xOffset), pos.y + (15.f*yOffset)), vel));
+		playerBullets.push_back(new Bullet("media/bullet.png", sf::Vector2f(pos.x + (30.f*xOffset), pos.y + (30.f*yOffset)), vel));
+		playerBullets.push_back(new Bullet("media/bullet.png", sf::Vector2f(pos.x - (15.f*xOffset), pos.y - (15.f*yOffset)), vel));
+		playerBullets.push_back(new Bullet("media/bullet.png", sf::Vector2f(pos.x - (30.f*xOffset), pos.y - (30.f*yOffset)), vel));
 		shotDone = true;
 	}
 
